b0bb86025dbddda96ac794c7126d5d69c83369f9
NikitaUmrihin <Nikitaumrihin@duck.com>
2025-04-06 14:20:36 +0300

    Added border collision, player graphics, debug mode, and FPS control

--- eggstinction/script.js
+++ eggstinction/script.js
@@ -1,12 +1,10 @@
 // Constants for game settings
-
 const MAX_ATTEMPTS = 500;       // Max number of attempts to place obstacles
 const MAX_OBSTACLES = 5;        // Max number of obstacles in the game
-
 const OBSTACLE_RADIUS = 50;     // Radius of obstacles
 const PLAYER_RADIUS = 30;       // Radius of the player
+const TOP_MARGIN = window.innerHeight *0.333;
 
-const TOP_MARGIN = window.innerHeight * 0.3333;
 
 window.addEventListener('load', function ()     // Waits for the whole page to load before running the code
 { 
@@ -52,27 +50,53 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
             // Differences between player and mouse
             this.dx = 0;
             this.dy = 0;
+
+            // Set color and image for the player 
+            this.color = "yellow";
+            this.image = document.getElementById("player-" + this.color);
+
+            // Define player sprite dimensions  
+            this.spriteWidth = 255;
+            this.spriteHeight = 256;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+
+            // Sprite location
+            this.spriteX;
+            this.spriteY;
+
+            // Frame for player animation
+            this.frameX = 0;
+            this.frameY = 2;
         }
 
         // Draws the player on the canvas
         draw(context) {
 
-            // Draw a circle at the player’s position
-            context.beginPath();
-            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
-            
-            // save() & restore() allows us to apply globalAlpha only to fill()
-            context.save();
-            context.globalAlpha = 0.25;     // Opacity
-            context.fill()                  // Fill the circle
-            context.restore();
-            context.stroke()
-            
-            // Draw a line from player’s position to mouse position 
-            context.beginPath();
-            context.moveTo(this.collisionX, this.collisionY);
-            context.lineTo(this.game.mouse.x, this.game.mouse.y);
-            context.stroke()
+            context.drawImage(this.image,
+                this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
+                this.spriteWidth, this.spriteHeight,
+                this.spriteX, this.spriteY - 100,
+                this.width, this.height);
+
+            if (this.game.debug) {
+                // Draw circle
+                context.beginPath();
+                context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+                // save() & restore() allows us to apply globalAlpha only to fill()
+                context.save();
+                context.globalAlpha = 0.25;
+                context.fill()
+                context.restore();
+                context.stroke()
+
+                // Draw line
+                context.beginPath();
+                context.moveTo(this.collisionX, this.collisionY);
+                context.lineTo(this.game.mouse.x, this.game.mouse.y);
+                context.stroke()
+
+            }
 
         }
 
@@ -84,20 +108,51 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
             // Calculate distance from mouse to player
             const distance = Math.sqrt(this.dx**2 + this.dy**2);
 
-            // Calculate speed (zero incase distance is undefined)
-            this.speedX = (this.dx)/distance || 0;
-            this.speedY = (this.dy)/distance || 0;
-            
-            // Move the player
+            // Calculate angle (in radians) between player and the mouse location
+            const angle = Math.atan2(this.dy, this.dx);
+
+            // Choose sprite frame based on angle
+            if (angle < -2.74 || angle > 2.74) this.frameY = 6;
+            else if (angle < -1.96) this.frameY = 7;
+            else if (angle < -1.17) this.frameY = 0;
+            else if (angle < -0.39) this.frameY = 1;
+            else if (angle < 0.39) this.frameY = 2;
+            else if (angle < 1.17) this.frameY = 3;
+            else if (angle < 1.96) this.frameY = 4;
+            else if (angle < 2.74) this.frameY = 5;
+            else if (angle < 2.74) this.frameY = 5;
+
             if (distance > this.speedModifier) {
-                this.collisionX += this.speedX * this.speedModifier;
-                this.collisionY += this.speedY * this.speedModifier;
-            }
-            // Stop moving if player is close to the mouse 
-            else {
+                // Calculate speed (zero incase distance is undefined)
+                this.speedX = (this.dx) / distance || 0;
+                this.speedY = (this.dy) / distance || 0;
+            } else {    // Stop moving if player is close to the mouse
                 this.speedX = 0;
                 this.speedY = 0;
             }
+
+            // Move the player
+            this.collisionX += this.speedX * this.speedModifier;
+            this.collisionY += this.speedY * this.speedModifier;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5;
+
+            //  Horizontal boundaries
+            //  Left edge
+            if (this.collisionX < this.collisionRadius)
+                this.collisionX = this.collisionRadius;
+            //  Right edge
+            else if (this.collisionX > this.game.width - this.collisionRadius)
+                this.collisionX = this.game.width - this.collisionRadius;
+
+            //  Vertical boundaries
+            //  Top edge
+            if (this.collisionY < TOP_MARGIN + this.collisionRadius)
+                this.collisionY = TOP_MARGIN + this.collisionRadius
+            //  Bottom edge
+            else if (this.collisionY > this.game.height - this.collisionRadius)
+                this.collisionY = this.game.height - this.collisionRadius
+
 
             // Adjust player position after checking for collisions with obstacles
             this.game.obstacles.forEach(obstacle => {
@@ -149,20 +204,23 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
             context.drawImage(this.image,
                 this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
                 this.spriteWidth, this.spriteHeight,
-                this.spriteX, this.spriteY-80,
+                this.spriteX, this.spriteY - 80,
                 this.width, this.height);
 
-            // Draw circle
-            context.beginPath();
-            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
-            
-            // save() & restore() allows us to apply globalAlpha only to fill()
-            context.save();
-            context.fillStyle='red';
-            context.globalAlpha = 0.15;
-            context.fill()
-            context.restore();
-            context.stroke()
+            if (this.game.debug) {
+                // Draw circle
+                context.beginPath();
+                context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+
+                // save() & restore() allows us to apply globalAlpha only to fill()
+                context.save();
+                context.fillStyle = 'red';
+                context.globalAlpha = 0.1;
+                context.fill()
+                context.restore();
+                context.stroke()
+
+            }
         }
     }
 
@@ -173,22 +231,39 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
             this.canvas = canvas;
             this.width = this.canvas.width;
             this.height = this.canvas.height;
-
+            
             // Create a player instance
             this.player = new Player(this);
-
+            
             // Initialize obstacles array
             this.numOfObstacles = MAX_OBSTACLES;
             this.obstacles = [];
-            
+
             // Mouse properties (start at canvas center)
             this.mouse = {
-                x:          this.width * 0.5,
-                y:          this.height * 0.5,
-                pressed:    false
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
             }
+
+            // Flag for debug mode
+            this.debug = true;
             
-            // _____ 🐭 Mouse Listeners 🐭_____
+            /// Game timer and FPS settings 
+            this.fps = 60;
+            this.timer = 0;
+            this.interval = 1000 / this.fps;
+
+
+            // _____ 👂 Keyboard Listeners 👂_____
+            // Event listener for button 'd' press to toggle debug mode
+            window.addEventListener('keydown', (e) => {
+                if (e.key === 'd') this.debug = !this.debug
+            });
+            // _____ 👂 __________________ 👂_____
+
+
+            // ______ 🐭 Mouse Listeners 🐭______
             // Event listener for mouse press
             window.addEventListener('mousedown', (e) => {
                 // console.log("down:", e.offsetX, e.offsetY);
@@ -213,16 +288,24 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
                     this.mouse.y = e.offsetY;
                 }
             });
-            // _____ 🐭 _______________ 🐭_____
+            // ______ 🐭 _______________ 🐭______
+
         }
 
         // Render method to update and draw game elements
-        render(context) {
-            this.player.draw(context);
-            this.player.update();
-            this.obstacles.forEach(obstacle => obstacle.draw(context))
-        }
+        render(context, deltaTime) {
+            if (this.timer > this.interval) {
+                // Clear canvas
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                this.timer = 0;
+                this.obstacles.forEach(obstacle => obstacle.draw(context))
+                this.player.draw(context);
+                this.player.update();
+            }
 
+            this.timer += deltaTime;
+
+        }
         // Check for collisions between two objects
         checkCollision(a,b) {
             const dx = a.collisionX - b.collisionX;
@@ -238,9 +321,7 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
 
             // While we haven't reached the max number of obstacles and attempts limit
             while (this.obstacles.length < this.numOfObstacles && attempts < MAX_ATTEMPTS) {
-                
                 let overlap = false;
-
                 let testObstacle = new Obstacle(this);
 
                 // Check all existing obstacles to see if the new obstacle overlaps with them
@@ -260,23 +341,22 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
                     }
 
                 });
-                
-                
-                const margin = 2* testObstacle.collisionRadius
 
+                const margin = 2.25 * testObstacle.collisionRadius
+                
                 // Check if the obstacle is within the bounds of the canvas (with margin)
                 let insideGame = testObstacle.spriteX > 0 &&
                     testObstacle.spriteX < this.width - testObstacle.width &&
                     testObstacle.collisionY > TOP_MARGIN + margin &&
                     testObstacle.collisionY < this.height - margin;
-                
-                
+
+
                 if (!overlap && insideGame) {
                     this.obstacles.push(testObstacle);
                 }
 
                 attempts++;
-                
+
             }
         }
     }
@@ -284,19 +364,20 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
     // Create a new game
     const game = new Game(canvas);
     game.init();
-    
+
+    let lastTime = 0;
 
     // Animation loop to continuously update and render the game
-    function animate() {
-        // Clear canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         // Render game elements
-        game.render(ctx);
+        game.render(ctx, deltaTime);
         // Call animate recursively
         requestAnimationFrame(animate);
     }
 
     // Start animation loop
-    animate(); 
+    animate(0);
 
 });
