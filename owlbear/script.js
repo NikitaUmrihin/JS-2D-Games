58d5b34e6d5619e1b948299d2938e87ea4deb697
NikitaUmrihin <Nikitaumrihin@duck.com>
2025-04-14 14:57:44 +0300

    Initial owlbear commit

---
+++ owlbear/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function ()     // Waits for the whole page to load before running the code
+{ 
+    // Get the canvas and overlay elements
+    const canvas = this.document.getElementById('canvas1');
+    const overlay = this.document.getElementById('overlay');
+
+});
