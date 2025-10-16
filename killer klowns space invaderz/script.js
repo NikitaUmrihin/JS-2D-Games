8c119b41bd6e1d44628349e6462120337ab4e7e0
NikitaUmrihin <Nikitaumrihin@duck.com>
2025-10-16 05:51:34 +0300

    Added movable player object

--- killer klowns space invaderz/script.js
+++ killer klowns space invaderz/script.js
@@ -1,14 +1,74 @@
+class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 100;
+        this.height = 100;
+        this.x = this.game.width / 2 - this.width / 2; 
+        this.y = this.game.height - this.height;
+
+        this.speed = 10;
+    }
+
+    draw(context) {
+        context.fillRect(this.x, this.y, this.width, this.height);    
+    }
+
+    update(){
+        // Move player according to pressed keys
+        if(this.game.keys.indexOf('ArrowLeft') > -1) 
+            this.x -= this.speed;
+        if(this.game.keys.indexOf('ArrowRight') > -1) 
+            this.x += this.speed;
+        
+        // Set player boundaries
+        if(this.x < 0) 
+            this.x = 0;
+        if(this.x > this.game.width - this.width) 
+            this.x = this.game.width - this.width;
+    }
+}
+
+
+// _____________________________________________________________________________
+// _____________________________________________________________________________
+
+
 class Game {
     constructor(canvas) {
         this.canvas = canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+
+        this.keys = [];
+        this.player = new Player(this);
+        
+        window.addEventListener('keydown', e => {
+            
+            if(this.keys.indexOf(e.key) === -1)
+                this.keys.push(e.key);
+            console.log(this.keys);
+        });
+        
+        window.addEventListener('keyup', e => {
+            if(this.keys.indexOf(e.key) !== -1)
+                this.keys.splice(this.keys.indexOf(e.key), 1);
+        });
+        
+    }
+
+
+    render(context){
+        this.player.draw(context);
+        this.player.update();
     }
 
 }
 
+// _____________________________________________________________________________
+// _____________________________________________________________________________
 
-window.addEventListener('load', function ()     // Waits for the whole page to load before running the code
+// Waits for the whole page to load before running the code
+window.addEventListener('load', function ()     
 { 
     // Get the canvas elements
     const canvas = this.document.getElementById('canvas1');
@@ -22,6 +82,14 @@ window.addEventListener('load', function ()     // Waits for the whole page to l
 
     // Create a new game
     const game = new Game(canvas);
+    
+    
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
     
 
 });
