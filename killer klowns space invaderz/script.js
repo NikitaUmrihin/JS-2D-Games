a16187d24e0af8178d0fac5d66ec3fd0a7b8492d
NikitaUmrihin <Nikitaumrihin@duck.com>
2025-10-17 02:07:07 +0300

    Added enemy wave grid

--- killer klowns space invaderz/script.js
+++ killer klowns space invaderz/script.js
@@ -1,3 +1,93 @@
+const ENEMY_SIZE = 75;
+
+class Enemy {
+    constructor(game, positionX, positionY) {
+        this.game = game;
+        this.width = ENEMY_SIZE;
+        this.height = ENEMY_SIZE;
+
+        // Actual position
+        this.x = this.game.width / 2 - this.width / 2; 
+        this.y = this.game.height - this.height;
+
+        // Position of enemy within the wave matrix
+        this.positionX = positionX;
+        this.positionY = positionY;
+
+        this.speed = 10;
+    }
+    
+    draw(context) {
+        context.strokeRect(this.x, this.y, this.width, this.height);    
+    }
+
+    update(x, y){
+        this.x = x + this.positionX;
+        this.y = y + this.positionY;
+    }
+    
+}
+
+
+// _____________________________________________________________________________
+// _____________________________________________________________________________
+
+class Wave{
+    constructor(game) {
+        this.game = game;
+        this.width = ENEMY_SIZE * this.game.columns;
+        this.height = ENEMY_SIZE * this.game.rows;
+        this.x = 0; 
+        this.y = -this.height;
+        this.speed = 5;
+        this.enemies = [];
+        this.create();        
+    }
+
+    render(context){
+
+        context.strokeStyle = 'white';
+
+        if(this.y < 0)
+            this.y += this.speed;
+
+        this.speedY = 0;
+
+        context.strokeRect(this.x, this.y, this.width, this.height);
+        
+        // Bounce off walls
+        if (this.x < 0 || this.x + this.width > this.game.width){
+            this.speed *= -1;
+            this.y += ENEMY_SIZE * 0.5;
+        }
+        
+        this.x += this.speed;
+
+        this.enemies.forEach(enemy => {
+            enemy.update(this.x, this.y);
+            enemy.draw(context);
+        });
+        
+    }
+
+    create(){
+        for (let y = 0; y < this.game.rows; y++){
+            for (let x = 0; x < this.game.columns; x++){
+                let enemyX = x * ENEMY_SIZE;
+                let enemyY = y * ENEMY_SIZE;
+                this.enemies.push(new Enemy(this.game, enemyX, enemyY));
+            }
+
+
+        }
+    }
+
+}
+
+// _____________________________________________________________________________
+// _____________________________________________________________________________
+
+
 class Player {
     constructor(game) {
         this.game = game;
@@ -10,6 +100,7 @@ class Player {
     }
 
     draw(context) {
+        context.fillStyle = 'white';
         context.fillRect(this.x, this.y, this.width, this.height);    
     }
 
@@ -47,11 +138,11 @@ class Missile {
         this.y = game.height - this.height;
 
         this.speed = 10;
-
         this.available = true;
     }
 
     draw(context){
+        // if missile not available -> its been shot -> draw it
         if(!this.available){
             context.fillRect(this.x, this.y, this.width, this.height);
         }
@@ -94,6 +185,14 @@ class Game {
         this.numberofMissiles = 5; 
         this.createMissiles();
         
+
+        this.rows = 3;
+        this.columns = 5;
+        
+
+        this.waves = [];
+        this.waves.push(new Wave(this));
+
         window.addEventListener('keydown', e => {
             if(this.keys.indexOf(e.key) === -1)
                 this.keys.push(e.key);
@@ -115,6 +214,9 @@ class Game {
         this.missiles.forEach(missile => {
             missile.update();
             missile.draw(context);
+        });
+        this.waves.forEach(wave => {
+            wave.render(context);
         });
     }
 
